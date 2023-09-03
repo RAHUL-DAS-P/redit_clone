bf96f5ac5a15ee858281adef1b4506be506a30f3
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-09-03 20:18:09 +0530

    added the fpdart package

--- lib/features/controller/auth_controller.dart
+++ lib/features/controller/auth_controller.dart
@@ -1,1 +1,24 @@
-class AuthController {}
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:redit_clone/features/repository/auth_repository.dart';
+
+final authControllerProvider = Provider<AuthController>(
+  (ref) {
+    return AuthController(
+      authRepository: ref.read(
+        authRepositoryProvider,
+      ),
+    );
+  },
+);
+
+class AuthController {
+  final AuthRepository _authRepository;
+
+  AuthController({
+    required AuthRepository authRepository,
+  }) : _authRepository = authRepository;
+
+  void signInWithGoogle() {
+    _authRepository.signInWithGoogle();
+  }
+}
--- lib/features/repository/auth_repository.dart
+++ lib/features/repository/auth_repository.dart
@@ -1,6 +1,19 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:redit_clone/core/constants/constants.dart';
+import 'package:redit_clone/core/constants/firebase_constants.dart';
+import 'package:redit_clone/core/providers/firebase_providers.dart';
+import 'package:redit_clone/model/user_model.dart';
+
+final authRepositoryProvider = Provider<AuthRepository>((ref) {
+  return AuthRepository(
+    firestore: ref.read(firestoreProvider),
+    auth: ref.read(authProvider),
+    googleSignIn: ref.read(googleSignInProvider),
+  );
+});
 
 class AuthRepository {
   final FirebaseFirestore _firestore;
@@ -15,21 +28,40 @@ class AuthRepository {
         _firestore = firestore,
         _googleSignIn = googleSignIn;
 
+  CollectionReference get _user =>
+      _firestore.collection(FirebaseConstants.usersCollection);
+
   void signInWithGoogle() async {
     try {
+      // Sign in with Google using GoogleSignIn
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
       final googleAuth = await googleUser?.authentication;
 
+      // Create GoogleAuthProvider credential
       final credential = GoogleAuthProvider.credential(
         accessToken: googleAuth?.accessToken,
         idToken: googleAuth?.idToken,
       );
 
+      // Sign in to Firebase Authentication using the Google credential
       UserCredential userCredential =
           await _auth.signInWithCredential(credential);
-      print(userCredential.user?.email);
-    } catch (e) {
-      print(e.toString());
-    }
+
+      UserModel userModel;
+
+      if (userCredential.additionalUserInfo!.isNewUser) {
+        userModel = UserModel(
+          name: userCredential.user!.displayName ?? "User",
+          profilePic: userCredential.user!.photoURL ?? Constants.avatarDefault,
+          banner: Constants.bannerDefault,
+          uid: userCredential.user!.uid,
+          isAuthenticated: true,
+          karma: 0,
+          awards: [],
+        );
+
+        await _user.doc(userModel.uid).set(userModel.toMap());
+      }
+    } catch (e) {}
   }
 }
