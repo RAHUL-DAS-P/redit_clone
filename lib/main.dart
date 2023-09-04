4d0bff1f5bfe228dffaeeadc98ac914b70562c68
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-09-04 14:30:16 +0530

    optimised the main

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:redit_clone/features/screens/login_screen.dart';
+import 'package:redit_clone/router.dart';
 import 'package:redit_clone/theme/pallete.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:redit_clone/firebase_options.dart';
+import 'package:routemaster/routemaster.dart';
 
 void main(List<String> args) async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -22,10 +23,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: Pallete.darkModeAppTheme,
-      home: const LoginScreen(),
+      routerDelegate: RoutemasterDelegate(routesBuilder: (_) => loggedOutRoute),
+      routeInformationParser: const RoutemasterParser(),
     );
   }
 }
