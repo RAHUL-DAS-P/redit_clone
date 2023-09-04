8777647e6cc9c3dffe60a4184d27460c3b4ad1f2
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-09-04 14:29:41 +0530

    added the loginroute

--- lib/features/controller/auth_controller.dart
+++ lib/features/controller/auth_controller.dart
@@ -1,12 +1,20 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 import 'package:redit_clone/core/utils.dart';
 import 'package:redit_clone/features/repository/auth_repository.dart';
 import 'package:redit_clone/model/user_model.dart';
 
 final userProvider = StateProvider<UserModel?>((ref) => null);
+
+final authStateChangeProvider = StreamProvider((ref) {
+  return ref.watch(authControllerProvider.notifier).authStateChange;
+});
+
+final getUserDataProvider = StreamProvider.family((ref, String uid) {
+  return ref.watch(authControllerProvider.notifier).getUserData(uid);
+});
 
 final authControllerProvider = StateNotifierProvider<AuthController, bool>(
   (ref) {
@@ -30,6 +38,8 @@ class AuthController extends StateNotifier<bool> {
         _ref = ref,
         super(false);
 
+  Stream<User?> get authStateChange => _authRepository.authStateChange;
+
   void signInWithGoogle(BuildContext context) async {
     state = true;
     final user = await _authRepository.signInWithGoogle();
@@ -39,5 +49,9 @@ class AuthController extends StateNotifier<bool> {
       (userModel) =>
           _ref.read(userProvider.notifier).update((state) => userModel),
     );
+  }
+
+  Stream<UserModel?> getUserData(String uid) {
+    return _authRepository.getUserData(uid);
   }
 }
