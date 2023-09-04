6469fbb5f8f717c0fd014c71d64a14ec1e207e86
RAHUL-DAS-P <rahul2003710@gmail.com>
2023-09-04 11:26:22 +0530

    saving everything before changing the web settings

--- lib/features/controller/auth_controller.dart
+++ lib/features/controller/auth_controller.dart
@@ -1,7 +1,12 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/widgets.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import 'package:redit_clone/core/utils.dart';
 import 'package:redit_clone/features/repository/auth_repository.dart';
+import 'package:redit_clone/model/user_model.dart';
+
+final userProvider = StateProvider<UserModel?>((ref) => null);
 
 final authControllerProvider = Provider<AuthController>(
   (ref) {
@@ -9,22 +14,27 @@ final authControllerProvider = Provider<AuthController>(
       authRepository: ref.read(
         authRepositoryProvider,
       ),
+      ref: ref,
     );
   },
 );
 
 class AuthController {
   final AuthRepository _authRepository;
+  final Ref _ref;
 
   AuthController({
     required AuthRepository authRepository,
-  }) : _authRepository = authRepository;
+    required Ref ref,
+  })  : _authRepository = authRepository,
+        _ref = ref;
 
   void signInWithGoogle(BuildContext context) async {
     final user = await _authRepository.signInWithGoogle();
     user.fold(
       (l) => showSnackBar(context, l.message),
-      (r) => null,
+      (userModel) =>
+          _ref.read(userProvider.notifier).update((state) => userModel),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:redit_clone/features/screens/login_screen.dart';
 import 'package:redit_clone/theme/pallete.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -9,7 +10,11 @@ void main(List<String> args) async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const MyApp());
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
